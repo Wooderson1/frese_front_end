73b45893b7d11f228d18fbc5b4ed0711f2896853
carter <carterjsherman@gmail.com>
2023-08-22 13:58:32 -0400

    calendar

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -8,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarPage implements OnInit {
 
   constructor() { }
-
+  getDateCal() {
+    const date = new Date();
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+    const year = date.getFullYear().toString();
+    return `${month}${day}${year}`;
+  }
+  getCalendarSrc() {
+    return `https://storage.googleapis.com/frese-product-images/${this.getDateCal()}calendar.jpg?ignoreCache=1`
+  }
   ngOnInit() {
   }
 
